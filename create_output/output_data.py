e1acedbf2e10544aa6b8d0d381cb754c920753e6
Ignacio Rojas <iarojas2@uc.cl>
2021-08-23 13:50:59 -0400

    feat: output data to excel

---
+++ create_output/output_data.py
@@ -0,0 +1,35 @@
+from openpyxl import Workbook, load_workbook
+import datetime;
+from pathlib import Path
+
+def create_excel(integral_values, output_path="") -> tuple:
+    """recives a list of lists with shape:
+    [integral_value, start_integral, end_integral]
+    and returns and creates an excel file with the data in the
+    output path"""
+
+    # Create workbook
+    wb = Workbook(write_only=True)
+    ws = wb.create_sheet()
+    ws.title = "results"
+
+    # Write colum names
+    colum_names = ["integral_value", "start_integral", "end_integral"]
+    ws.append(colum_names)
+
+    # Write data
+    for i in range(len(integral_values)):
+        ws.append(integral_values[i])
+
+    timestamp = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+    file_name = "output_" + timestamp + ".xlsx"
+    output_dir_path = "output_data"
+    # Create output folder if it doesnt exists
+    Path(output_dir_path).mkdir(parents=True, exist_ok=True)
+    # Save excel file
+    wb.save(output_dir_path + "/" + file_name)
+
+
+if __name__ == "__main__":
+    print("Executing read_data")
+    read_file("./data/01_ PVE 1_Importacion de datos filtrados_con graficos.xlsm")
