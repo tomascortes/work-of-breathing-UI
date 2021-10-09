cef61a5c756ad4472e16e2ff336eca1c97d1a560
tomascortes <ticortes@uc.cl>
2021-10-09 17:13:28 -0300

    feat(ui_improved)

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ import sys
 from PyQt5.QtWidgets import QApplication
 from src.frontend.main_window import MainWindow
 
-if __name__ == "__main__":
+def main():
     app = QApplication([])
     m = MainWindow()
     sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    main()
--- src/frontend/file_selector.py
+++ src/frontend/file_selector.py
@@ -1,18 +1,16 @@
-from PyQt5 import uic
+from PyQt5.QtWidgets import  QWidget, QMainWindow, QLabel, QPushButton, QVBoxLayout, QHBoxLayout, QSpacerItem, QSizePolicy
 from PyQt5.QtCore import pyqtSignal
 from PyQt5.QtWidgets import QFileDialog
 import os
 
 
-window_name, base_class = uic.loadUiType("src/frontend/ui_files/menu.ui")
 
-
-class Menu(window_name, base_class):
+class Menu(QMainWindow):
     trigger = pyqtSignal(tuple)
 
     def __init__(self):
         super().__init__()
-        self.setupUi(self)
+        self.init_layout()
 
         self.push_button_ready.clicked.connect(self.continue_to_processing)
         self.push_button_file_selector.clicked.connect(self.path_file_selector)
@@ -47,3 +45,32 @@ class Menu(window_name, base_class):
             directory=os.getcwd(),
         )
         self.excel_path = response[0]
+
+    def init_layout(self):
+        self.setWindowTitle("Selección archivo")
+        self.setGeometry(50, 50, 500, 700)
+        self.statusBar().showMessage('')
+
+        widget = QWidget(self)
+        self.setCentralWidget(widget)
+
+        hlay = QHBoxLayout(widget)
+        hlay.addItem(QSpacerItem(1000, 10, QSizePolicy.Expanding))
+        vlay = QVBoxLayout()
+        self.label_instructions = QLabel('Seleccione el archivo excel a utilizar', self)
+        self.push_button_file_selector = QPushButton('Abrir archivo', self)
+        self.label_path_info = QLabel('No hay archivo seleccionado', self)
+        self.label_path = QLabel('', self)
+        self.push_button_ready = QPushButton('Continuar', self)
+
+        vlay.addWidget(self.label_instructions)
+        vlay.addWidget(self.push_button_file_selector)
+        vlay.addWidget(self.label_path_info)
+        vlay.addWidget(self.label_path)
+        vlay.addWidget(self.push_button_ready)
+        vlay.addItem(QSpacerItem(1000, 10, QSizePolicy.Expanding))
+
+
+
+        hlay.addLayout(vlay)
+        hlay.addItem(QSpacerItem(1000, 10, QSizePolicy.Expanding))
