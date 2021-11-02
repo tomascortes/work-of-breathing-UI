8d88b1e9972fd4fe549393b99645f0512e7fa44f
IngisRedd <iarojas2@uc.cl>
2021-11-01 23:19:44 -0300

    finished processor class

--- src/backend/peak_finder/signal_processing.py
+++ src/backend/peak_finder/signal_processing.py
@@ -148,150 +148,125 @@ def get_pes_peaks(data, big_sigma=300, small_sigma=25, dist_from_pl=0, dist=1, p
     return (pes_peaks, very_smoothed_sgnl, less_smoothed_sgnl)
 
 # New functions 1/11/2021:
-def get_smoothed_signal(data, smoothing_sigma = 2.1) -> list:
+class SignalProcessor:
     """
-    Returns a smoothed version of original signal data, filtered with a gaussian filter with sigma smoothing_sigma
-    """
-    signal = np.array(data)
-
-    return gaussian_filter(signal, smoothing_sigma)
-
-def get_straight_signal_peaks(data, smoothing_sigma=2.1, big_sigma=300, dist_from_pl=0, dist=75, prom=1) -> tuple:
-    """
-    Returns tuple of arrays of positions of local maxima and minima of each cycle of the signal.
-
+    Class for all signal processing functionality.
     * Inputs:
-    data:                   array of signal data.
-    smoothing_sigma:        the initial data is smoothed with sigma smoothing_sigma before finding the straight_peaks.
-    big_sigma:              positions are considered peaks or antipeaks if they are found over or under the signal smoothed with big_sigma.
-    dist_from_pl:           will not return any peaks or antipeaks in  smoothed signal with big_sigma +- dist_from_pl o.
-    dist:                   minimum distance between peaks or antipeaks.
-    prom:                   minimum prominence (vertical distance between the peak and its lowest contour line) of peaks or antipeaks.
+        data:                   array of signal data.
+        smoothing_sigma:        the initial data is smoothed with sigma smoothing_sigma before finding the straight_peaks.
+        big_sigma:              positions are considered peaks or antipeaks if they are found over or under the signal smoothed with big_sigma.
+        dist_from_pl:           will not return any peaks or antipeaks in smoothed signal with big_sigma +- dist_from_pl o.
+        dist:                   minimum distance between straight peaks or antipeaks.
+        prom:                   minimum prominence for straigt_peaks(vertical distance between the peak and its lowest contour line) of peaks or antipeaks.
     """
-    # ys corresponds to an array of the signal values
-    ys = np.array(data)
-
-    # smoothe signal ys with smoothing_sigma to ignore noise-derived peaks
-    smoothed_ys = gaussian_filter(ys, smoothing_sigma)
-    # signal smoothed with big_sigma
-    very_smoothed_sgnl = gaussian_filter(ys, big_sigma)
-
-
-    # positions for positive peaks (local maxima)
-    straight_peaks, _ = find_peaks(smoothed_ys, height=(
-        very_smoothed_sgnl + dist_from_pl, np.amax(ys)), distance=dist, prominence=prom)
-    # positions for negative peaks (local minima)
-    straight_antipeaks, _ = find_peaks(-smoothed_ys, height=(
-        -very_smoothed_sgnl + dist_from_pl, np.amax(-ys)), distance=dist, prominence=prom)
-
-    return straight_peaks, straight_antipeaks
-
-def get_inflection_points(data, smoothing_sigma=2.1, big_sigma=300, small_sigma=25, dist_from_pl=0) -> tuple:
-    """
-    Returns x positions of inflection points of the signal, estimated as the intersections between the original signal
-        smoothed with sigma big_sigma and sigma small_sigma.
-
-    * Inputs:
-    data:                   array of signal data.
-    smoothing_sigma:        the initial data is smoothed with sigma smoothing_sigma to clean certain noise.
-    big_sigma, small_sigma: the function returns the first peaks before and after each inflection point of the signal,
-        wich are aproximated as the intersection of a smoothed version of the original signal with sigma big_sigma and 
-        other version smoothed with small_sigma.
-    dist_from_pl:           will not return any peaks under smoothed signal with big_sigma + dist_from_pl.
-    """
-
-    # ys corresponds to an array of the signal values
-    ys = np.array(data)
-
-    # signal smoothed with big_sigma
-    very_smoothed_sgnl = gaussian_filter(ys, big_sigma)
-    # signal smoothed with small_sigma
-    less_smoothed_sgnl = gaussian_filter(ys, small_sigma)
-
-    # Positive when signal in mount, negative in valley:
-    mount_or_valley = np.sign(less_smoothed_sgnl - very_smoothed_sgnl)
-    # Positions where curve less_smoothed_signl crosses curve very_smoothed_sgnl
-    inflection_points = np.where(np.diff(mount_or_valley))[0]
-    # Whe get the x positions of the intersections when derivative is positive or negative
-    if mount_or_valley[0] > 0:
-        inflection_points_pos_der = inflection_points[::2]
-        inflection_points_neg_der = inflection_points[1::2]
-    else:
-        inflection_points_pos_der = inflection_points[1::2]
-        inflection_points_neg_der = inflection_points[::2]
+    def __init__(self, data, smoothing_sigma=2.1, big_sigma=300, small_sigma=25, dist_from_pl=0, dist=75,  prom=1):
+        # self.signal corresponds to an array of the signal values
+        self.signal = np.array(data)
+        # smoothed signal with smoothing_sigma to ignore noise-derived peaks
+        self.smoothed_signal = self.get_smoothed_signal(self.signal, smoothing_sigma)
+        # signal smoothed with big_sigma
+        self.big_smoothed_signal = self.get_smoothed_signal(self.signal, big_smoothing_sigma)
+        # signal smoothed with small_sigma
+        self.small_smoothed_signal = self.get_smoothed_signal(self.signal, small_smoothing_sigma)
+        
+        # Values for calculating straight_signal_peaks
+        self.dist_from_pl = dist_from_pl
+        self.dist = dist
+        self.prom = prom
 
 
-    return (inflection_points_pos_der, inflection_points_neg_der)
+        # ALL positions for positive peaks (local maxima)
+        self.all_peaks, _ = find_peaks(self.smoothed_signal, height=(
+            self.big_smoothed_signal + self.dist_from_pl, np.amax(self.signal)), distance=1)
+        # ALL positions for negative peaks (local minima)
+        self.all_antipeaks, _ = find_peaks(-self.smoothed_signal, height=(
+            -self.big_smoothed_signal + self.dist_from_pl, np.amax(-self.signal)), distance=1)
 
-def get_sided_peaks(all_peaks, inflection_points, side) -> list:
-    """
-    Returns array of positions of first local peak of each valley/mount of the signal, coming from side "side".
-    """
-    sc_len = len(inflection_points)
-    peaks = []
-    cnt = 0
-    if side == "right":
-        for i, peak_xpos in enumerate(all_peaks):
-            if peak_xpos >= inflection_points[cnt]:
-                peaks.append(all_peaks[i-1])
-                cnt += 1
-                if cnt == sc_len:
-                    break
-    elif side == "left":
-        all_peaks = all_peaks[::-1]
-        inflection_points = inflection_points[::-1]
-        for i, peak_xpos in enumerate(all_peaks):
-            if peak_xpos <= inflection_points[cnt]:
-                peaks.append(all_peaks[i-1])
-                cnt += 1
-                if cnt == sc_len:
-                    break
-        peaks = peaks[::-1]
-    
-    return peaks
+        self.ip_pos, self.ip_neg = self.get_inflection_points()
 
-def get_all_sided_signal_peaks(data, smoothing_sigma=2.1, big_sigma=300, small_sigma=25, dist_from_pl=0) -> tuple:
-    """
-    Obtains all kinds of peaks from an input signal.
+        self.left_peaks = self.get_sided_peaks(side="left")
+        self.right_peaks = self.get_sided_peaks(side="right")
+        self.left_antipeaks = self.get_sided_peaks(side = "left", antipeak = True)
+        self.right_antipeaks = self.get_sided_peaks(side = "right", antipeak = True)
 
-    * Inputs:
-    data:                   array of signal data.
-    smoothing_sigma:        the initial data is smoothed with sigma smoothing_sigma to clean certain noise.
-    big_sigma, small_sigma: the function returns the first peaks before and after each inflection point of the signal,
-        wich are aproximated as the intersection of a smoothed version of the original signal with sigma big_sigma and 
-        other version smoothed with small_sigma.
-    dist_from_pl:           will not return any peaks under smoothed signal with big_sigma + dist_from_pl.
+    def get_smoothed_signal(self, smoothing_sigma) -> list:
+        """
+        Returns a smoothed version of original signal data, filtered with a gaussian filter with sigma smoothing_sigma
+        """
 
+        return gaussian_filter(self.signal, smoothing_sigma)
 
-    * Outputs:
-    left_peaks:             array of positions of first local maxima of each mount of the signal.
-    right_peaks:            array of positions of last local maxima of each mount of the signal.
-    left_antipeaks:         array of positions of first local minima of each valley of the signal.
-    right_antipeaks:        array of positions of last local minima of each valley of the signal.
-    """
+    def get_straight_signal_peaks(self, antipeak = False) -> tuple:
+        """
+        Returns tuple of arrays of positions of local maxima or minima of each cycle of the signal.
+        """
 
-    # smoothe signal ys with smoothing_sigma to ignore noise-derived peaks
-    smoothed_ys = gaussian_filter(ys, smoothing_sigma)
-    # signal smoothed with big_sigma
-    very_smoothed_sgnl = gaussian_filter(ys, big_sigma)
+        if not antipeak:
+            # positions for positive peaks (local maxima)
+            peaks, _ = find_peaks(self.smoothed_signal, height=(
+                self.very_smoothed_sgnl + self.dist_from_pl, np.amax(self.signal)), distance=self.dist, prominence=self.prom)
+        else:
+            # positions for negative peaks (local minima)
+            peaks, _ = find_peaks(-self.smoothed_signal, height=(
+                -self.very_smoothed_sgnl + self.dist_from_pl, np.amax(-self.signal)), distance=self.dist, prominence=self.prom)
 
-    # ALL positions for positive peaks (local maxima)
-    all_peaks, _ = find_peaks(smoothed_ys, height=(
-        very_smoothed_sgnl + dist_from_pl, np.amax(ys)), distance=1)
-    # ALL positions for negative peaks (local minima)
-    all_antipeaks, _ = find_peaks(-smoothed_ys, height=(
-        -very_smoothed_sgnl + dist_from_pl, np.amax(-ys)), distance=1)
+        return peaks
 
-    ip_pos, ip_neg = get_inflection_points(data, smoothing_sigma=smoothing_sigma, big_sigma=big_sigma, small_sigma=small_sigma, dist_from_pl=dist_from_pl)
+    def get_inflection_points(self) -> tuple:
+        """
+        Returns two arrays of x positions of inflection points of the signal, estimated as the intersections between
+            the original signal smoothed with sigma big_sigma and sigma small_sigma. First array is when derivative is
+            positive, second is when first derivative is negative.
+        """
 
-    left_peaks = get_sided_peaks(all_peaks,ip_pos, "left")
-    right_peaks = get_sided_peaks(all_peaks, ip_neg, "right")
-    left_antipeaks = get_sided_peaks(all_antipeaks, ip_pos, "left")
-    right_antipeaks = get_sided_peaks(all_antipeaks, ip_neg, "right")
+        # Positive when signal in mount, negative in valley:
+        mount_or_valley = np.sign(self.small_smoothed_signal - self.big_smoothed_signal)
+        # Positions where curve less_smoothed_signl crosses curve very_smoothed_sgnl
+        inflection_points = np.where(np.diff(mount_or_valley))[0]
+        # Whe get the x positions of the intersections when derivative is positive or negative
+        if mount_or_valley[0] > 0:
+            inflection_points_pos_der = inflection_points[::2]
+            inflection_points_neg_der = inflection_points[1::2]
+        else:
+            inflection_points_pos_der = inflection_points[1::2]
+            inflection_points_neg_der = inflection_points[::2]
 
+        return (inflection_points_pos_der, inflection_points_neg_der)
 
-    return (left_peaks, right_peaks,
-        left_antipeaks, right_antipeaks)
+    def get_sided_peaks(self, side, antipeak = False) -> list:
+        """
+        Returns array of positions of first local peak of each valley/mount of the signal, coming from side "side".
+        """
+        if antipeak:
+            all_peaks = self.all_antipeaks
+        else:
+            all_peaks = self.all_peaks
+        
+        sc_len = len(inflection_points)
+        peaks = []
+        cnt = 0
+        if side == "right":
+            inflection_points = self.ip_neg
+            for i, peak_xpos in enumerate(all_peaks):
+                if peak_xpos >= inflection_points[cnt]:
+                    peaks.append(all_peaks[i-1])
+                    cnt += 1
+                    if cnt == sc_len:
+                        break
+        elif side == "left":
+            inflection_points = self.ip_pos
+            all_peaks = all_peaks[::-1]
+            inflection_points = inflection_points[::-1]
+            for i, peak_xpos in enumerate(all_peaks):
+                if peak_xpos <= inflection_points[cnt]:
+                    peaks.append(all_peaks[i-1])
+                    cnt += 1
+                    if cnt == sc_len:
+                        break
+            peaks = peaks[::-1]
+        
+        return peaks
+
 
 
 if __name__ == '__main__':
